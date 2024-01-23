d152b875dd544db0c14f674c2e9ca400e2a02f31
Abhishek kumar <101728039+2731999@users.noreply.github.com>
2024-01-23 19:19:26 +0530

    Add files via upload

--- serverr/index.js
+++ serverr/index.js
@@ -8,7 +8,13 @@ const messageRoute = require('./routes/messagesRoute');
 const app  = express();
 require("dotenv").config();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: [""],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 app.use('/api/auth', userRoutes);
