144b6e3907a3c93c83c3ce08ce20c899b54dcdd5
Abhishek kumar <101728039+2731999@users.noreply.github.com>
2024-02-04 10:47:07 +0530

    Add files via upload

--- publicc/src/utils/APIroutes.js
+++ publicc/src/utils/APIroutes.js
@@ -1,4 +1,4 @@
-const host  = 'http://127.0.0.1:5000';
+const host  = 'chat-app-fuu4.vercel.app';
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
